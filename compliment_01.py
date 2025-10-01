468ccd35bd8e29b99ada8c2b4e7ef4528331b6af
Crytpt1cN1ck <nsheppard4@fingerlakes.edu>
2025-10-01 00:40:03 +0000

    Compliment Lab Attempt 1

--- compliment_01.py
+++ compliment_01.py
@@ -1,8 +1,9 @@
 # FILE NAME - compliment_01.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Nicholas Sheppard
+# DATE: 09/30/2025
+# BRIEF DESCRIPTION:  In this program, the user is prompted whether they want a compliment or not. If they choose "yes", 
+# then a compliment is given. Either way, the user is thanked for participating.
 
 
 
@@ -15,7 +16,18 @@
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+def main ():
+    compliment_01()
 
+def compliment_01():
+    answer = input("Would you like a compliment? ")
+    if answer == "yes":
+        print("You have wonderful eyes.")
+
+    print("Thank you for playing.")
+
+
+main()
 
 
 
@@ -67,15 +79,12 @@ Thank you for playing.
 
 1. On a scale of 1 to 10 (where 10 is the hardest), how would you rate this lab?
 
-
+I'd say it was pretty easy. I give it a 3.
 
 
 2. What was the hardest part of this lab?
 
-
-
-
-
+In my if condition I forgot to put yes in quotes, so I kept getting an error.
 
 
 '''
